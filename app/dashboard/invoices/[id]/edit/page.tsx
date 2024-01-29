fc66b06bf54a4d8de39a7409979e313a36d72b28
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2024-01-29 18:11:55 +0900

    🥅 invoiceのエラーハンドリング

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 type Params = {
   id: string;
@@ -12,6 +13,11 @@ export default async function Page({ params }: { params: Params }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoic e) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
