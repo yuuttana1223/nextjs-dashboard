1befcba293eed4759eb1614a19de3e637cab0957
yuuttana1223 <64090349+yuuttana1223@users.noreply.github.com>
2024-02-02 16:11:13 +0900

    🔍 metadataを追加

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,11 +6,16 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
 
 type SearchParams = {
   query?: string;
   page?: string;
 };
+
+export const metadata = {
+  title: 'Invoices',
+} satisfies Metadata;
 
 export default async function Page({
   searchParams,
